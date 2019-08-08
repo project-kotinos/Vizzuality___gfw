7021f4e52bf012e05418438eb5e2f1b54ca08a11
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 15:04:18 -0700

    add MagickCore

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev
+apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev MagickCore
 gem install bundler -v 2.0.1
 # install
 bundle install
