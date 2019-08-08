58cf34e3fec8d74518599d96b445416b2ecc851e
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 15:15:10 -0700

    add libmagickwand-dev

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev MagickCore
+apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev libmagickwand-dev
 gem install bundler -v 2.0.1
 # install
 bundle install
