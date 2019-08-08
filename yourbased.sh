5cef1c747659c767b7c750ec72ee69dd7c42f4d3
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 16:39:23 -0700

    uninstall cmdtest again

--- yourbased.sh
+++ yourbased.sh
@@ -5,6 +5,7 @@ apt purge cmdtest
 apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev libmagickwand-dev
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update
 apt-get -y install yarn
 gem install bundler -v 2.0.1
 # install
