5be3f12b5e9ebff2726440da1bdf5ac93d090175
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 15:49:12 -0700

    add yarn, node, and aws env

--- yourbased.sh
+++ yourbased.sh
@@ -2,8 +2,12 @@
 set -ex
 export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev libmagickwand-dev
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get -y install yarn
 gem install bundler -v 2.0.1
 # install
 bundle install
 # script
+
 yarn ci
