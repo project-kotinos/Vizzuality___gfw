6f7f9874379f2a14d3ffdd3820732b7a11ed2890
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 14:53:05 -0700

    typo

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install libcurl4-openssl-dev libxml2-dev
+apt-get update && apt-get install -y libcurl4-openssl-dev libxml2-dev
 gem install bundler -v 2.0.1
 # install
 bundle install
