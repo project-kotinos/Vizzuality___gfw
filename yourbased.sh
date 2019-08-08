a30beb92f99790a20f650d2a129f97c9ed53ad1d
wmuenigneer <adam.d.bell@icloud.io>
2019-08-08 14:42:43 -0700

    add libcurl4

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install libcurl
+apt-get update && apt-get install libcurl4
 gem install bundler -v 2.0.1
 # install
 bundle install
